f1c48814ef0f3cfa2b7722a86626bd5d91b19b66
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-03-25 22:04:27 -0700

    added index.js file, created delete and createlist functions

---
+++ index.js
@@ -0,0 +1,38 @@
+//create html item shopping list
+function createItem(item){
+    return `<li>
+    <span class="shopping-item">${item}</span>
+    <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+    </li>`
+}
+
+//add item to go to shopping list 
+function createList(){
+    $("#js-shopping-list-form").on('submit' , function(e){
+        e.preventDefault();
+    
+        let item = $("#shopping-list-entry").val();
+        console.log(item);
+        // push to list
+        $(".shopping-list").append($(createItem(item)));
+        $("#shopping-list-entry").val('');
+    });
+}
+
+// delete item from list when hit delete 
+function deleteItem(){
+    $('.shopping-list').on('click', '.shopping-item-delete', function(e){
+        e.preventDefault();
+        $(e.target).closest('li').remove();
+    });
+}
+
+$(createList);
+$(deleteItem);
